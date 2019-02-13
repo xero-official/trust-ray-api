3e0799ae152581cd4cade6d86a391640d3345039
Jan <janonoratu@yahoo.it>
2019-02-13 19:53:32 +0100

    some changes

--- src/controllers/TokenPriceController.ts
+++ src/controllers/TokenPriceController.ts
@@ -150,7 +150,7 @@ export class TokenPriceController {
             "POA": "poa-network",
             "CLO": "callisto-network",
             "GO" : "gochain",
-            "ETHO": "ether1"
+            "ETHO": "Ether-1"
         }
 
         return tokens.map((token: IToken) => {
