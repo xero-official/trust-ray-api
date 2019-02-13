fa06312ebca394f6cac57af9d71f4d4e1de04780
Jan <janonorati@yahoo.it>
2019-02-13 21:48:04 +0100

    Update TokenPriceController.ts

--- src/controllers/TokenPriceController.ts
+++ src/controllers/TokenPriceController.ts
@@ -150,7 +150,7 @@ export class TokenPriceController {
             "POA": "poa-network",
             "CLO": "callisto-network",
             "GO" : "gochain",
-            "ETHO" : "ether-1"
+            "ETHO" : "Ether1"
          }
 
 
