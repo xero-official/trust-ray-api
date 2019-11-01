14af3402ca0010d58fe4c0b7817af8a2b3d7160e
FallenGravity <ethanvb55@gmail.com>
2019-11-01 12:16:14 +0200

    Test #1

--- src/controllers/RedirectController.ts
+++ src/controllers/RedirectController.ts
@@ -6,148 +6,154 @@ import * as qs from "qs";
 
 export class Redirect {
 
-    public getTransactions = async (req: Request, res: Response) => {
-        const json = {docs: []}
-        const networkId = req.params.networkId
-        const query = this.createQuery(req.query)
-        const url = `${Nodes[networkId]}${Endpoints.Transactions}${query}`
-        const transactions = await this.getAddressTokens({url})
+  public getTransactions = async (req: Request, res: Response) => {
+    const json = { docs: [] }
+    const networkId = req.params.networkId
+    const query = this.createQuery(req.query)
+    const url = `${Nodes[networkId]}${Endpoints.Transactions}${query}`
+    const transactions = await this.getAddressTokens({ url })
 
-        if (Array.isArray(transactions)) {
-            transactions.forEach(transaction => {
-                transaction.coin = CoinTypeIndex[networkId]
-                json.docs.push(transaction)
-            })
-            return res.json(json)
+    if (Array.isArray(transactions)) {
+      transactions.forEach(transaction => {
+        transaction.coin = CoinTypeIndex[networkId]
+        json.docs.push(transaction)
+      })
+      return res.json(json)
+    }
+    res.json(json)
+  }
+
+  public getAddressAllTokens = async (req, res) => {
+    const json = { docs: [] }
+    const networks = Object.keys(CoinTypeIndex)
+    const bodyNetworks = Object.keys(req.body)
+    const commonNetworks = this.getCommonNetworks(networks, bodyNetworks)
+
+    await BluebirbPromise.map(commonNetworks, async (networkIndex) => {
+      const networkId = CoinTypeIndex[networkIndex]
+      const addresses: string[] = req.body[networkIndex]
+      const url = `${Nodes[networkId]}${Endpoints.Tokens}`
+
+      await BluebirbPromise.map(addresses, async (address) => {
+        const tokens: any = await this.getAddressTokens({ url, params: { address: address.toLowerCase() } })
+        if (Array.isArray(tokens)) {
+          tokens.forEach(token => {
+            token.contract.coin = parseInt(networkIndex)
+            token.contract.type = "ERC20"
+            json.docs.push(token)
+          })
         }
-        res.json(json)
+      })
+    })
+
+    res.json(json)
+  }
+
+  public getTokensList = async (req, res) => {
+    const tokens = { docs: [] }
+    const { query, networks, verified = true } = req.query
+    const queryNetworks: string[] = networks ? networks.split(",") : networks
+
+    if (!query || !queryNetworks) {
+      return res.json(tokens)
     }
 
-    public getAddressAllTokens = async (req, res) => {
-        const json = {docs: []}
-        const networks = Object.keys(CoinTypeIndex)
-        const bodyNetworks = Object.keys(req.body)
-        const commonNetworks = this.getCommonNetworks(networks, bodyNetworks)
+    const queryNetworksId = queryNetworks.map(net => CoinTypeIndex[net])
+    const commonNetworks = this.getCommonNetworks(queryNetworksId, Object.keys(CoinTypeIndex))
 
-        await BluebirbPromise.map(commonNetworks, async (networkIndex) => {
-            const networkId = CoinTypeIndex[networkIndex]
-            const addresses: string[] = req.body[networkIndex]
-            const url = `${Nodes[networkId]}${Endpoints.Tokens}`
+    await BluebirbPromise.map(commonNetworks, async (network) => {
+      const url: string = `${Nodes[network]}${Endpoints.TokensList}`
+      const networkTokenList = await this.getAddressTokens({ url, params: { query, verified } })
 
-            await BluebirbPromise.map(addresses, async (address) => {
-                const tokens: any = await this.getAddressTokens({url, params: {address: address.toLowerCase()}})
-                if (Array.isArray(tokens)) {
-                    tokens.forEach(token => {
-                        token.contract.coin = parseInt(networkIndex)
-                        token.contract.type = "ERC20"
-                        json.docs.push(token)
-                    })
-                }
-            })
+      if (Array.isArray(networkTokenList)) {
+        networkTokenList.forEach(token => {
+          token.coin = parseInt(CoinTypeIndex[network])
+          token.type = "ERC20"
+          tokens.docs.push(token)
+        });
+      }
+    })
+
+    res.json(tokens)
+  }
+
+  public register = async (req: Request, res: Response) => {
+    const registrResults = []
+    const { deviceID, token, type } = req.body
+    const networks = Object.keys(req.body.networks)
+    const commonNetworks = this.getCommonNetworks(networks, Object.keys(CoinTypeIndex))
+
+    try {
+      await BluebirbPromise.map(commonNetworks, async (network) => {
+        const url = `${Nodes[CoinTypeIndex[network]]}${Endpoints.RegisterDevice}`
+        const wallets = req.body.networks[network]
+        const data = { deviceID, token, type, wallets }
+        const registered = await axios.post(url, data).then(res => res.data).catch(e => {
+          // when request failed
+          console.log(e)
         })
+        registrResults.push(registered)
+      })
 
-       res.json(json)
+      this.sendJSONresponse(res, 200, registrResults)
+    } catch (error) {
+      console.error(`Error registering device`, error)
+      this.sendJSONresponse(res, 400, { error: "Error regestering device" })
     }
 
-    public getTokensList = async (req, res) => {
-        const tokens = {docs: []}
-        const { query, networks, verified = true } = req.query
-        const queryNetworks: string[] = networks ? networks.split(",") : networks
+  }
 
-        if (!query || !queryNetworks) {
-            return res.json(tokens)
-        }
+  public unregister = async (req: Request, res: Response) => {
+    const unregisterResults = []
+    const { deviceID, token, networks, type } = req.body
+    const networksToUnregister = this.getSupportedCoinIndex()
 
-        const queryNetworksId = queryNetworks.map(net => CoinTypeIndex[net])
-        const commonNetworks = this.getCommonNetworks(queryNetworksId, Object.keys(CoinTypeIndex))
-
-        await BluebirbPromise.map(commonNetworks, async (network) => {
-            const url: string = `${Nodes[network]}${Endpoints.TokensList}`
-            const networkTokenList = await this.getAddressTokens({url, params: {query, verified}})
-
-            if (Array.isArray(networkTokenList)) {
-                networkTokenList.forEach(token => {
-                    token.coin = parseInt(CoinTypeIndex[network])
-                    token.type = "ERC20"
-                    tokens.docs.push(token)
-                });
-            }
+    try {
+      await BluebirbPromise.map(networksToUnregister, async (networkIndex) => {
+        const coin = CoinTypeIndex[networkIndex]
+        const url = `${Nodes[coin]}${Endpoints.UnegisterDevice}`
+        const data = { deviceID, token, networks, type }
+        const unregistered = await axios.post(url, data).then(res => res.data).catch(e => {
+          // when request failed
+          console.log(e)
         })
+        unregisterResults.push(unregistered)
+      })
 
-        res.json(tokens)
+      this.sendJSONresponse(res, 200, unregisterResults)
+    } catch (error) {
+      console.error(`Error registering device`, error)
+      this.sendJSONresponse(res, 500, { error: error.toString() })
     }
+  }
 
-    public register = async (req: Request, res: Response) => {
-        const registrResults = []
-        const {deviceID, token, type} = req.body
-        const networks = Object.keys(req.body.networks)
-        const commonNetworks = this.getCommonNetworks(networks, Object.keys(CoinTypeIndex))
+  private getSupportedCoinIndex() {
+    const keys = Object.keys(CoinTypeIndex).filter(k => typeof CoinTypeIndex[k as any] === "number")
+    const values = keys.map(k => CoinTypeIndex[k as any])
+    return values;
+  }
 
-        try {
-           await BluebirbPromise.map(commonNetworks, async (network) => {
-                const url = `${Nodes[CoinTypeIndex[network]]}${Endpoints.RegisterDevice}`
-                const wallets = req.body.networks[network]
-                const data = { deviceID, token, type, wallets }
-                const registered = await axios.post(url, data).then(res => res.data).catch(error=>{console.log("Fuck")});
-                registrResults.push(registered)
-           })
+  private sendJSONresponse(res: Response, status: number, content: any) {
+    return res.status(status).json(content)
+  }
 
-           this.sendJSONresponse(res, 200, registrResults)
-        } catch (error) {
-            console.error(`Error registering device`, error)
-            this.sendJSONresponse(res, 400, {error: "Error regestering device"})
-        }
+  public getCommonNetworks = (arr1: string[], arr2: string[]): string[] => {
+    return [arr1, arr2].shift().filter(v => [arr1, arr2].every(a => a.indexOf(v) !== -1))
+  }
 
-    }
+  public getAssets = (req: Request, res: Response) => {
+    const query = this.createQuery(req.query)
+    const url = `${Nodes.ether1}${Endpoints.Assets}${query}`
+    res.redirect(url)
+  }
 
-    public unregister = async (req: Request, res: Response) => {
-        const unregisterResults = []
-        const {deviceID, token, networks, type} = req.body
-        const networksToUnregister = this.getSupportedCoinIndex()
+  public createQuery = (query: any): string => {
+    return query ? `?${qs.stringify(query)}` : ``
+  }
 
-        try {
-            await BluebirbPromise.map(networksToUnregister, async (networkIndex) => {
-                const coin = CoinTypeIndex[networkIndex]
-                const url = `${Nodes[coin]}${Endpoints.UnegisterDevice}`
-                const data = {deviceID, token, networks, type}
-                const unregistered = await axios.post(url, data).then(res => res.data).catch(error=>{console.log("Fuck")});
-                unregisterResults.push(unregistered)
-            })
-
-            this.sendJSONresponse(res, 200, unregisterResults)
-        } catch (error) {
-            console.error(`Error registering device`, error)
-            this.sendJSONresponse(res, 500, {error: error.toString()})
-        }
-    }
-
-    private getSupportedCoinIndex () {
-        const keys = Object.keys(CoinTypeIndex).filter(k => typeof CoinTypeIndex[k as any] === "number")
-        const values = keys.map(k => CoinTypeIndex[k as any])
-        return values;
-    }
-
-    private sendJSONresponse(res: Response, status: number, content: any) {
-        return res.status(status).json(content)
-    }
-
-    public getCommonNetworks = (arr1: string[], arr2: string[]): string[] => {
-        return [arr1, arr2].shift().filter(v => [arr1, arr2].every(a => a.indexOf(v) !== -1))
-    }
-
-    public getAssets = (req: Request, res: Response) => {
-        const query = this.createQuery(req.query)
-        const url = `${Nodes.ether1}${Endpoints.Assets}${query}`
-        res.redirect(url)
-    }
-
-    public createQuery = (query: any): string => {
-        return query ? `?${qs.stringify(query)}` : ``
-    }
-
-    public getAddressTokens(args) {
-        const {url, params } = args
-        return axios({url, params}).then(res => res.data.docs ? res.data.docs : res.data)
-    }
+  public getAddressTokens(args) {
+    const { url, params } = args
+    return axios({ url, params }).then(res => res.data.docs ? res.data.docs : res.data)
+  }
 
 }
